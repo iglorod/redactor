ff4a4d198029353f0c05a45262128e44204ef27b
igor29 <igmatviytsev@gmail.com>
2020-04-19 23:14:28 +0300

    Updates

---
+++ precache-manifest.680375c9944d3d8c7f462fde6dee95c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bf3ad7f84ec4f2027ad07d3902aab6",
+    "url": "/redactor/index.html"
+  },
+  {
+    "revision": "4d75b8abda26b58c1e09",
+    "url": "/redactor/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "01e74e229c39ad9775e5",
+    "url": "/redactor/static/css/main.dd572e09.chunk.css"
+  },
+  {
+    "revision": "4d75b8abda26b58c1e09",
+    "url": "/redactor/static/js/2.4f505d9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/redactor/static/js/2.4f505d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e74e229c39ad9775e5",
+    "url": "/redactor/static/js/main.f74ce803.chunk.js"
+  },
+  {
+    "revision": "f66392a46047f7b69e6f",
+    "url": "/redactor/static/js/runtime-main.9f70cf7c.js"
+  }
+]);
